60d92e32767304e52cb2e87f36ed761146413721
trader0808 <arcmoon12@naver.com>
2022-06-17 08:05:07 +0900

    [ENH] Add comments for future me

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -30,17 +30,19 @@ typealias LumaListener = (luma: Double) -> Unit
 class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
 
-    private var imageCapture: ImageCapture? = null
+    private var imageCapture: ImageCapture? = null // ImageCapture? 이런식으로 ?가 붙으면 null이 가능하다는 의미
 
     private var videoCapture: VideoCapture<Recorder>? = null
     private var recording: Recording? = null
 
-    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var cameraExecutor: ExecutorService // lateinit은 나중에 초기화가 가능하다는 뜻. 다른 것과 달리 = 뭐뭐 가 없다
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(viewBinding.root)
+        setContentView(viewBinding.root) // AcivityMainBinding 은 activity_main이 자동으로 바뀐 형태. 다른 layout이면 그것들도 대문자로 합쳐져서 나옴
+        // viewBinding 하여서 안에 있는 객체들을 선언하지 않아도 쓸수있게 해준다 ( 버튼, 텍스트뷰 등 )
+        // ex) viewBinding.imageCaptureButton.setOnClickListener { takePhoto() }
 
         // Request camera permissions
         if (allPermissionsGranted()) {
@@ -48,18 +50,24 @@ class MainActivity : AppCompatActivity() {
         } else {
             ActivityCompat.requestPermissions(
                 this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
-        }
+        } // Request_code_permissions 는 원하는 숫자 지정하면 될거 같다고 한다.
 
         // Set up the listeners for take photo and video capture buttons
         viewBinding.imageCaptureButton.setOnClickListener { takePhoto() }
         viewBinding.videoCaptureButton.setOnClickListener { captureVideo() }
 
         cameraExecutor = Executors.newSingleThreadExecutor()
+        // 프로세스란 간단히 말해서 실행 중인 프로그램이다. 프로그램을 실행하면 os 로부터 실행에 필요한 자원(메모리)를 할당받아 프로세스가 된다.
+        // 프로세스는 프로그램을 수행하는 데 필요한 데이터와 메모리 등의 자원 그리고 쓰레드로 구성되어 있으며 프로세서의 자원을 이용해서 실제로 작업을 수앻하는 것이 바로 쓰레드다.
+        // 그래서 모든 프로세스에는 최소한 하나 이상의 쓰레드가 존재하며, 둘 이상의 쓰레드를 가진 프로세스를 멀티쓰레드 프로세스라고 한다.
+        // 멀티 쓰레딩은 하나의 프로세스 내에서 여러 쓰레드가 동시에 작업을 수행하는 것이 가능하다. 실제로는 한 개의 cpu가 한 번에 단 한가지 작업만 수행할 수 있기 떄문에 아주 짧은 시간 동안 여러 작업을 번갈아 가며
+        // 수행함으로써 동시에 여러 작업이 수행되는 것처럼 보이게 하는 것이다. ex) 메신저의 경우 채팅하면서 파일을 다운로드 받거나 음성대화를 나눌 수 있는 것이 멀티쓰레드
     }
 
     private fun takePhoto() {
         // Get a stable reference of the modifiable image capture use case
-        val imageCapture = imageCapture ?: return
+        val imageCapture = imageCapture ?: return // null일 경우 기본값을 주는 연산자 ?:
+        // first operand ? : second operand - first operand가 null이 아니면 first return, null 이면 second return
 
         // Create time stamped name and MediaStore entry.
         val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
@@ -69,7 +77,7 @@ class MainActivity : AppCompatActivity() {
             put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
             if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
                 put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
-            }
+            } // contentvalues 는 키와 값의 쌍 형태로 이용되는 클래스
         }
 
         // Create output options object which contains file + metadata
@@ -212,7 +220,15 @@ class MainActivity : AppCompatActivity() {
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
             baseContext, it) == PackageManager.PERMISSION_GRANTED
-    }
+    } // all은 array 함수로써 array의 모든 요소들이 전부 조건을 만족하면 true false를 내보내는 boolean
+    // it은 각 요소의 string을 차례대로 보여준다. (all 안에 있어서 그런듯)
+    // context는 어블리케이션 환경에 대한 글로벌 정보를 갖는 인터페이스. https://roomedia.tistory.com/entry/Android-Context%EB%9E%80-%EB%AC%B4%EC%97%87%EC%9D%BC%EA%B9%8C
+    // context는 application context와 activity context가 있으며 가까운걸 사용해야 한다.
+    // 싱글턴은 application context 사용해야 한다고 하는데 잘 이해가 안간다.
+    // context는 시스템을 사용하기 위한 정보와 도구가 담겨있는 기본 클레스
+    // basecontext는 안드로이드 컴포넌트들의 base class. 각 컴포넌트 들은 다른 context를 갖는다. basecontext 또는 this로 불러올 수 있다.
+    // application context는 앱과 관련된 핵심 기능을 담고 있는 클래스. 앱 전체에 하나의 인스턴스만 존재
+
 
     override fun onDestroy() {
         super.onDestroy()
@@ -230,9 +246,9 @@ class MainActivity : AppCompatActivity() {
             ).apply {
                 if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
                     add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                }
+                } // apply는 inline function에서 안쪽에 함수 넣어서 조건 맞으면 더 해주는 것
             }.toTypedArray()
-    }
+    } // companion static 인가봐 static은 중복되게 사용될 떄 하나의 메모리만 보게 하는 것
 
     override fun onRequestPermissionsResult(
         requestCode: Int, permissions: Array<String>, grantResults:
